1b5fbf8df5af06bbfefe1f1fcbb26ec5a981f314
rysmith <ryansmith05@gmail.com>
2015-05-28 18:23:45 -0700

    add welcome controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/index'
+
+  get 'welcome/index' => 'welcome#index', as: :welcome
+
+  get 'users/' => 'users#index', as: :users
+
+  get 'users/new' => 'users#new', as: :user_new
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #You can have the root of your site routed with "root"
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
