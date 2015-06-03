9decd60c6590e8ff195056f2dbacab12a7d6f90f
rysmith <ryansmith05@gmail.com>
2015-06-03 08:37:32 -0700

    create show comment view and method with corresponding links in comment index [issue #17]

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,8 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
 
-  #
+  #currently only the :comment_title and :comment_text are being saved
+  #need to update with line information
   def create
     comment = Comment.new(params.require(:comment).permit(:comment_title, :comment_text))
     if comment.save
@@ -20,12 +21,18 @@ class CommentsController < ApplicationController
     end
   end
 
+  #caputure one row of the Comments table and store it in @comment
+  #find is a method the Comment class gets from Active Record
+  #params is the @comment hash we are sending over to the show view
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
   def edit
   end
 
   def update
   end
 
-  def show
-  end
+
 end
