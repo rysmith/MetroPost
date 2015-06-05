520319e4b6bf0986dbc8380847858813a1ff914f
rysmith <ryansmith05@gmail.com>
2015-06-05 10:24:32 -0700

    allow user to create favorite lines [issue #24]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,10 +4,17 @@ class FavoritesController < ApplicationController
   end
 
   def new
+    @favorite = Userline.new
   end
 
   def create
-
+    user_line = Userline.new(params.require(:userline).permit(:line_id, :user_id))
+    current_user.userlines.push user_line
+    if user_line.save
+      redirect_to favorites_path
+    else
+      redirect_to new_favorite
+    end
   end
 
   def edit
