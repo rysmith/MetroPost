fff9a0ebf8e4f37ec1c390a715d27695d7237907
rysmith <ryansmith05@gmail.com>
2015-06-05 13:24:07 -0700

    comment and refactor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,41 @@
 class CommentsController < ApplicationController
 
-  #show all comments in the comment view
+
   def index
+
+    #show all comments in the comment view
     @comments = Comment.all
+
   end
 
-  #create a new comment object and send it over to the form
+
   def new
+
+    #create a new comment object and send it over to the form
     @comment = Comment.new
+
   end
 
   #currently only the :comment_title and :comment_text are being saved
   #need to update with line information
   def create
+
+    #receive params from view and add them to the db
     comment = Comment.new(params.require(:comment).permit(:comment_title, :comment_text))
+
+    #auto update the user_id foreign key with the current_user's user_id
     current_user.comments.push comment
+
     if comment.save
+
+      #if we have a successful save, show all comments
       redirect_to comments_path
+
     else
+
+      #else render the form again
       redirect_to new_comment
+
     end
   end
 
@@ -26,27 +43,38 @@ class CommentsController < ApplicationController
   #find is a method the Comment class gets from Active Record
   #params is the @comment hash we are sending over to the show view
   def show
+
     @comment = Comment.find(params[:id])
+
   end
 
   def edit
+
     @comment = Comment.find(params[:id])
+
   end
 
   def update
+
     @comment = Comment.find(params[:id])
+
     if @comment.update_attributes(params.require(:comment).permit(:comment_title, :comment_text, :user_id))
+
       redirect_to comments_path
+
     else
+
       render :edit
+
     end
   end
 
   def destroy
+
     @comment = Comment.find(params[:id])
     @comment.destroy
+
     redirect_to comments_path
+
   end
-
-
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,33 @@
 class FavoritesController < ApplicationController
+
+
   def index
+
+    #in the index view we are only showing the current_user's favorites
     @favorites = Userline.all
+
   end
 
+
   def new
+
+    #send over a new favorite object for the user to fill in
     @favorite = Userline.new
+
   end
 
   def create
+
+    #recieve the favorite object back from the view
     user_line = Userline.new(params.require(:userline).permit(:line_id, :user_id))
+
+    #auto update the user_id foreign key with the current_user's user_id
     current_user.userlines.push user_line
+
     if user_line.save
       redirect_to favorites_path
     else
       redirect_to new_favorite
     end
-  end
-
-  def edit
   end
 end
