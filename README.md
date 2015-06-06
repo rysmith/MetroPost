003c0814c724db0bfe0945cde4beeed9461ef185
rysmith <ryansmith05@gmail.com>
2015-06-05 21:18:41 -0700

    uncomment line seed data for production db seeding

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 MetroPost is a place to share information about the [LA Metro](http://www.metro.net/) rail service.  
 
-You can find it online at [www.metropost.la](http://www.metropost.la)
+You can find it online at [www.metropost.la](http://www.metropost.la).
 
 MetroPost is built with [Rails](http://rubyonrails.org/) and [Bootstrap](http://getbootstrap.com).  
 
-Data from [LA Metro](http://developer.metro.net/) and the NextBus [Public XML Feed](https://www.nextbus.com/xmlFeedDocs/NextBusXMLFeed.pdf)
+Data from [LA Metro](http://developer.metro.net/) and the NextBus [Public XML Feed](https://www.nextbus.com/xmlFeedDocs/NextBusXMLFeed.pdf).git 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,29 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-#seeded at 2015-05-30 06:43:22.825601
-# lines = Line.create([
-#     {
-#         line_id: 801,
-#         line_name: 'Metro Blue Line'
-#     },
-#     {
-#         line_id: 802,
-#         line_name: 'Metro Red Line'
-#     },
-#     {
-#         line_id: 803,
-#         line_name: 'Metro Green Line'
-#     },
-#     {
-#         line_id: 804,
-#         line_name: 'Metro Gold Line'
-#     },
-#     {
-#         line_id: 805,
-#         line_name: 'Metro Purple Line'
-#     },
-#     {
-#         line_id: 806,
-#         line_name: 'Metro Expo Line'
-#     }
-# ])
+lines = Line.create([
+    {
+        line_id: 801,
+        line_name: 'Metro Blue Line'
+    },
+    {
+        line_id: 802,
+        line_name: 'Metro Red Line'
+    },
+    {
+        line_id: 803,
+        line_name: 'Metro Green Line'
+    },
+    {
+        line_id: 804,
+        line_name: 'Metro Gold Line'
+    },
+    {
+        line_id: 805,
+        line_name: 'Metro Purple Line'
+    },
+    {
+        line_id: 806,
+        line_name: 'Metro Expo Line'
+    }
+])
